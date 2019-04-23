74bf45b63849d78caf892e5318602f2073bc8a55
yuki7070 <yu.yu.yu.shine@gmail.com>
2019-04-24 08:26:53 +0900

    TSFile関数の定義

--- mpeg.go
+++ mpeg.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -12,44 +10,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	data := map[uint8]map[uint16]map[uint8][]ShortEventDescriptor{}
-	pidBuf := map[uint16][]byte{}
-	//tableBuf := map[uint8][]byte{}
+
 	ids := []byte{0x4E, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f}
 
-	for {
-		p := make([]byte, 188)
-		err = binary.Read(f, binary.BigEndian, &p)
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			panic(err)
-		}
-		packet := Packet(p)
-		prev, next := packet.Parser()
-		if prev == nil && next == nil {
-			continue
-		}
-
-		PID := packet.PID()
-		if prev != nil {
-			pidBuf[PID] = append(pidBuf[PID], prev...)
-			continue
-		}
-		if buf, ok := pidBuf[PID]; ok {
-			table := Table(buf)
-			temp := table.Parser(ids, data)
-			if temp != nil {
-				data = temp
-			}
-		}
-		if next != nil {
-			pidBuf[PID] = next
-		}
-	}
+	data := TSFile(f, ids)
 	fmt.Println(data)
-	fmt.Println("=============================")
-	for _, t := range data {
-		fmt.Println(t)
-	}
 }
--- parser.go
+++ parser.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/binary"
+	"io"
+	"os"
+)
+
 func (p Packet) Parser() ([]byte, []byte) {
 	PID := p.PID()
 	switch PID {
@@ -19,4 +25,42 @@ func (p Packet) Parser() ([]byte, []byte) {
 		return nil, nil
 	}
 	return nil, nil
+}
+
+func TSFile(f *os.File, ids []byte) map[uint8]map[uint16]map[uint8][]ShortEventDescriptor {
+	data := map[uint8]map[uint16]map[uint8][]ShortEventDescriptor{}
+
+	pidBuf := map[uint16][]byte{}
+
+	for {
+		p := make([]byte, 188)
+		err := binary.Read(f, binary.BigEndian, &p)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		packet := Packet(p)
+		prev, next := packet.Parser()
+		if prev == nil && next == nil {
+			continue
+		}
+
+		PID := packet.PID()
+		if prev != nil {
+			pidBuf[PID] = append(pidBuf[PID], prev...)
+			continue
+		}
+		if buf, ok := pidBuf[PID]; ok {
+			table := Table(buf)
+			temp := table.Parser(ids, data)
+			if temp != nil {
+				data = temp
+			}
+		}
+		if next != nil {
+			pidBuf[PID] = next
+		}
+	}
+	return data
 }
